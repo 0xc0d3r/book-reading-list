8aafb1c65db690d6bc9b0a6537e3449ea6df27c3
Anesh Parvatha <anesh@cybereyelabs.io>
2020-02-20 11:58:31 +0530

    Connect mongodb

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://0xc0d3r:Anesh123@cluster0-lg16l.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.once('open', () => {
+  console.log('[+] Connected to database!');
+});
 
 app.use(
   '/graphql',
